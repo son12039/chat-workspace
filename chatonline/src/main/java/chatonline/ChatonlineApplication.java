d4a4dbaa889747a2bb70d7290fbcf9ae9d868737
son12039 <sonson12039@gmail.com>
2024-09-02 12:49:46 +0900

    1

--- chatonline/src/main/java/chatonline/ChatonlineApplication.java
+++ chatonline/src/main/java/chatonline/ChatonlineApplication.java
@@ -2,12 +2,19 @@ package chatonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatonlineApplication {
+public class ChatonlineApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatonlineApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChatonlineApplication.class);
 	}
 
 }
